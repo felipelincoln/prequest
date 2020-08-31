96fd2f3de2ecc062902c6c2c58969477fda052aa
Felipe Lincoln <felipelincoln@usp.br>
2020-08-31 07:00:25 -0300

    include tests

--- mix.exs
+++ mix.exs
@@ -57,7 +57,9 @@ defmodule Prequest.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:ex_doc, "~>0.22", only: :dev, runtime: false}
+      {:ex_doc, "~>0.22", only: :dev, runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.8", only: :dev}
     ]
   end
 
