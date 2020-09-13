f07dc05258ea8b7d2084e7802a02c1318d195632
Felipe Lincoln <felipelincoln@usp.br>
2020-09-13 10:30:04 -0300

    add topics

--- test/prequest/cms_test.exs
+++ test/prequest/cms_test.exs
@@ -219,4 +219,63 @@ defmodule Prequest.CMSTest do
       assert %Ecto.Changeset{} = CMS.change_view(view)
     end
   end
+
+  describe "topics" do
+    alias Prequest.CMS.Topic
+
+    @valid_attrs %{name: "some name"}
+    @update_attrs %{name: "some updated name"}
+    @invalid_attrs %{name: nil}
+
+    def topic_fixture(attrs \\ %{}) do
+      {:ok, topic} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> CMS.create_topic()
+
+      topic
+    end
+
+    test "list_topics/0 returns all topics" do
+      topic = topic_fixture()
+      assert CMS.list_topics() == [topic]
+    end
+
+    test "get_topic!/1 returns the topic with given id" do
+      topic = topic_fixture()
+      assert CMS.get_topic!(topic.id) == topic
+    end
+
+    test "create_topic/1 with valid data creates a topic" do
+      assert {:ok, %Topic{} = topic} = CMS.create_topic(@valid_attrs)
+      assert topic.name == "some name"
+    end
+
+    test "create_topic/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = CMS.create_topic(@invalid_attrs)
+    end
+
+    test "update_topic/2 with valid data updates the topic" do
+      topic = topic_fixture()
+      assert {:ok, %Topic{} = topic} = CMS.update_topic(topic, @update_attrs)
+      assert topic.name == "some updated name"
+    end
+
+    test "update_topic/2 with invalid data returns error changeset" do
+      topic = topic_fixture()
+      assert {:error, %Ecto.Changeset{}} = CMS.update_topic(topic, @invalid_attrs)
+      assert topic == CMS.get_topic!(topic.id)
+    end
+
+    test "delete_topic/1 deletes the topic" do
+      topic = topic_fixture()
+      assert {:ok, %Topic{}} = CMS.delete_topic(topic)
+      assert_raise Ecto.NoResultsError, fn -> CMS.get_topic!(topic.id) end
+    end
+
+    test "change_topic/1 returns a topic changeset" do
+      topic = topic_fixture()
+      assert %Ecto.Changeset{} = CMS.change_topic(topic)
+    end
+  end
 end
