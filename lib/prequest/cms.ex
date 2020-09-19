8c67bc2d0a968040b07a3014ace02a78a1458aec
Felipe Lincoln <felipelincoln@usp.br>
2020-09-19 07:10:22 -0300

    make create_article build topics

--- lib/prequest/cms.ex
+++ lib/prequest/cms.ex
@@ -116,21 +116,27 @@ defmodule Prequest.CMS do
   end
 
   @doc """
-  Preload fields from an article
+  Preload fields from an article.
+
+  Raises `ArgumentError` if `fields` does not exist in `%Article{}`.
+
+  ## Examples
+
+      iex> preload_article!(article, :topics)
+      %Article{}
+
+      iex> preload_article!(article, [:views, :reports])
+      %Article{}
+
+      iex> preload_article!(article, :field)
+      ** (ArgumentError)
   """
   def preload_article!(%Article{} = article, fields) do
     Repo.preload(article, fields)
   end
 
   @doc """
-  Preload article's fields in a tuple pipeline
-  """
-  def preload_article({:ok, %Article{} = article}, fields) do
-    {:ok, Repo.preload(article, fields)}
-  end
-
-  @doc """
-  Creates a article.
+  Creates an article.
 
   ## Examples
 
@@ -142,374 +148,23 @@ defmodule Prequest.CMS do
 
   """
   def create_article(attrs \\ %{}) do
-    Ecto.Multi.new()
-    |> Ecto.Multi.insert(:articles, Article.changeset(%Article{}, attrs))
-    |> Ecto.Multi.insert(:topics, Topic.changeset(%Topic{}, ))
-    |> Repo.transaction()
-  end
-
-
-  alias Prequest.CMS.Topic
-
-  @doc """
-  """
-  def get_or_create_topic(name) when is_binary(name) do
-    case Repo.get_by Topic, name: name do
-      %Topic{} = topic -> topic
-      nil -> 
-        %Topic{}
-        |> Topic.changeset(%{name: name})
-        |> Repo.insert()
-    end
-  end
-
-  def get_or_create_topic(names) when is_list(names) do
-    Enum.map(names, &get_or_create_topic/1)
-  end
-
-
-
-
-
-
-
-
-
-
-  ######################
-
-  @doc """
-  Updates a article.
-
-  ## Examples
-
-      iex> update_article(article, %{field: new_value})
-      {:ok, %Article{}}
-
-      iex> update_article(article, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_article(%Article{} = article, attrs) do
-    article
+    %Article{}
     |> Article.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a article.
-
-  ## Examples
-
-      iex> delete_article(article)
-      {:ok, %Article{}}
-
-      iex> delete_article(article)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_article(%Article{} = article) do
-    Repo.delete(article)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking article changes.
-
-  ## Examples
-
-      iex> change_article(article)
-      %Ecto.Changeset{data: %Article{}}
-
-  """
-  def change_article(%Article{} = article, attrs \\ %{}) do
-    Article.changeset(article, attrs)
-  end
-
-  alias Prequest.CMS.Report
-
-  @doc """
-  Returns the list of reports.
-
-  ## Examples
-
-      iex> list_reports()
-      [%Report{}, ...]
-
-  """
-  def list_reports do
-    Repo.all(Report)
-  end
-
-  @doc """
-  Gets a single report.
-
-  Raises `Ecto.NoResultsError` if the Report does not exist.
-
-  ## Examples
-
-      iex> get_report!(123)
-      %Report{}
-
-      iex> get_report!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_report!(id), do: Repo.get!(Report, id)
-
-  @doc """
-  Creates a report.
-
-  ## Examples
-
-      iex> create_report(%{field: value})
-      {:ok, %Report{}}
-
-      iex> create_report(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_report(attrs \\ %{}) do
-    %Report{}
-    |> Report.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:topics, build_topics(attrs))
     |> Repo.insert()
-  end
-
-  @doc """
-  Updates a report.
-
-  ## Examples
-
-      iex> update_report(report, %{field: new_value})
-      {:ok, %Report{}}
-
-      iex> update_report(report, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_report(%Report{} = report, attrs) do
-    report
-    |> Report.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a report.
-
-  ## Examples
-
-      iex> delete_report(report)
-      {:ok, %Report{}}
-
-      iex> delete_report(report)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_report(%Report{} = report) do
-    Repo.delete(report)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking report changes.
-
-  ## Examples
-
-      iex> change_report(report)
-      %Ecto.Changeset{data: %Report{}}
-
-  """
-  def change_report(%Report{} = report, attrs \\ %{}) do
-    Report.changeset(report, attrs)
-  end
-
-  alias Prequest.CMS.View
-
-  @doc """
-  Returns the list of view.
-
-  ## Examples
-
-      iex> list_view()
-      [%View{}, ...]
-
-  """
-  def list_view do
-    Repo.all(View)
-  end
-
-  @doc """
-  Gets a single view.
-
-  Raises `Ecto.NoResultsError` if the View does not exist.
-
-  ## Examples
-
-      iex> get_view!(123)
-      %View{}
-
-      iex> get_view!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_view!(id), do: Repo.get!(View, id)
-
-  @doc """
-  Creates a view.
-
-  ## Examples
-
-      iex> create_view(%{field: value})
-      {:ok, %View{}}
-
-      iex> create_view(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_view(attrs \\ %{}) do
-    %View{}
-    |> View.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a view.
-
-  ## Examples
-
-      iex> update_view(view, %{field: new_value})
-      {:ok, %View{}}
-
-      iex> update_view(view, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_view(%View{} = view, attrs) do
-    view
-    |> View.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a view.
-
-  ## Examples
-
-      iex> delete_view(view)
-      {:ok, %View{}}
-
-      iex> delete_view(view)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_view(%View{} = view) do
-    Repo.delete(view)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking view changes.
-
-  ## Examples
-
-      iex> change_view(view)
-      %Ecto.Changeset{data: %View{}}
-
-  """
-  def change_view(%View{} = view, attrs \\ %{}) do
-    View.changeset(view, attrs)
   end
 
   alias Prequest.CMS.Topic
 
-  @doc """
-  Returns the list of topics.
-
-  ## Examples
-
-      iex> list_topics()
-      [%Topic{}, ...]
-
-  """
-  def list_topics do
-    Repo.all(Topic)
+  # Retrieve a topic struct from database if it exists, or a map otherwise.
+  defp build_topics(%{topics: names} = _attrs) do
+    Enum.map(names, fn name ->
+      case Repo.get_by(Topic, name: name) do
+        %Topic{} = topic -> topic
+        nil -> %{name: name}
+      end
+    end)
   end
 
-  @doc """
-  Gets a single topic.
-
-  Raises `Ecto.NoResultsError` if the Topic does not exist.
-
-  ## Examples
-
-      iex> get_topic!(123)
-      %Topic{}
-
-      iex> get_topic!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_topic!(id), do: Repo.get!(Topic, id)
-
-  @doc """
-  Creates a topic.
-
-  ## Examples
-
-      iex> create_topic(%{field: value})
-      {:ok, %Topic{}}
-
-      iex> create_topic(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_topic(attrs \\ %{}) do
-    %Topic{}
-    |> Topic.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a topic.
-
-  ## Examples
-
-      iex> update_topic(topic, %{field: new_value})
-      {:ok, %Topic{}}
-
-      iex> update_topic(topic, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_topic(%Topic{} = topic, attrs) do
-    topic
-    |> Topic.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a topic.
-
-  ## Examples
-
-      iex> delete_topic(topic)
-      {:ok, %Topic{}}
-
-      iex> delete_topic(topic)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_topic(%Topic{} = topic) do
-    Repo.delete(topic)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking topic changes.
-
-  ## Examples
-
-      iex> change_topic(topic)
-      %Ecto.Changeset{data: %Topic{}}
-
-  """
-  def change_topic(%Topic{} = topic, attrs \\ %{}) do
-    Topic.changeset(topic, attrs)
-  end
+  defp build_topics(_attrs), do: []
 end
--- test/prequest/cms_test.exs
+++ test/prequest/cms_test.exs
@@ -5,7 +5,7 @@ defmodule Prequest.CMSTest do
   alias Prequest.CMS
 
   setup_all do
-    {:ok, user} = Accounts.create_user(%{username: "felipelincoln2"})
+    {:ok, user} = Accounts.create_user(%{username: "felipelincoln"})
     on_exit(fn -> Accounts.delete_user(user) end)
 
     %{user: user}
@@ -18,13 +18,14 @@ defmodule Prequest.CMSTest do
       cover: "some cover",
       topics: ["topic1", "topic2", "topic3"]
     }
-    @update_attrs %{
-      source: "some updated source",
-      title: "some updated title",
-      cover: "some updated cover",
-      topics: ["topic1.1", "topic2"]
-    }
-    @invalid_attrs %{source: nil, title: nil, cover: nil, user_id: nil}
+
+    #    @update_attrs %{
+    #      source: "some updated source",
+    #      title: "some updated title",
+    #      cover: "some updated cover",
+    #      topics: ["topic1.1", "topic2"]
+    #    }
+    #    @invalid_attrs %{source: nil, title: nil, cover: nil, user_id: nil}
 
     def article_fixture(%{user_id: _} = attrs) do
       {:ok, article} =
@@ -37,10 +38,8 @@ defmodule Prequest.CMSTest do
 
     test "get_article!/1 returns the article with given id", %{user: user} do
       article = article_fixture(%{user_id: user.id})
-      assert CMS.get_article!(article.id) == article
+      assert CMS.get_article!(article.id) |> CMS.preload_article!(:topics) == article
     end
-
-    ## FAILING
 
     test "preload_article!/2 returns the article with preloaded field", %{user: user} do
       article = article_fixture(%{user_id: user.id})
@@ -58,243 +57,12 @@ defmodule Prequest.CMSTest do
                %{user_id: user.id}
                |> Enum.into(@valid_attrs)
                |> CMS.create_article()
-               |> CMS.preload_article(:topics)
 
       assert article.source == @valid_attrs.source
       assert article.title == @valid_attrs.title
       assert article.cover == @valid_attrs.cover
       assert article.user_id == user.id
-      refute article.topics == []
-    end
-
-    ##############
-
-    test "create_article/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = CMS.create_article(@invalid_attrs)
-    end
-
-    test "update_article/2 with valid data updates the article", %{user: user} do
-      article = article_fixture(%{user_id: user.id})
-      assert {:ok, %CMS.Article{} = article} = CMS.update_article(article, @update_attrs)
-      assert article.source == "some updated source"
-      assert article.title == "some updated title"
-      assert article.cover == "some updated cover"
-    end
-
-    test "update_article/2 with invalid data returns error changeset", %{user: user} do
-      article = article_fixture(%{user_id: user.id})
-      assert {:error, %Ecto.Changeset{}} = CMS.update_article(article, @invalid_attrs)
-      assert article == CMS.get_article!(article.id)
-    end
-
-    test "delete_article/1 deletes the article", %{user: user} do
-      article = article_fixture(%{user_id: user.id})
-      assert {:ok, %CMS.Article{}} = CMS.delete_article(article)
-      assert_raise Ecto.NoResultsError, fn -> CMS.get_article!(article.id) end
-    end
-
-    test "change_article/1 returns a article changeset", %{user: user} do
-      article = article_fixture(%{user_id: user.id})
-      assert %Ecto.Changeset{} = CMS.change_article(article)
-    end
-  end
-
-  describe "reports" do
-    alias Prequest.CMS.Report
-
-    @valid_attrs %{message: "some message"}
-    @update_attrs %{message: "some updated message"}
-    @invalid_attrs %{article_id: nil}
-
-    def article_id do
-      {:ok, user} = Accounts.create_user(%{username: "felipelincoln"})
-
-      {:ok, article} =
-        CMS.create_article(%{title: "a", cover: "a", source: "a", user_id: user.id})
-
-      article.id
-    end
-
-    def report_fixture(attrs \\ %{}) do
-      {:ok, report} =
-        attrs
-        |> Map.merge(%{article_id: article_id()})
-        |> Enum.into(@valid_attrs)
-        |> CMS.create_report()
-
-      report
-    end
-
-    test "list_reports/0 returns all reports" do
-      report = report_fixture()
-      assert CMS.list_reports() == [report]
-    end
-
-    test "get_report!/1 returns the report with given id" do
-      report = report_fixture()
-      assert CMS.get_report!(report.id) == report
-    end
-
-    test "create_report/1 with valid data creates a report" do
-      attrs = Map.merge(@valid_attrs, %{article_id: article_id()})
-      assert {:ok, %Report{} = report} = CMS.create_report(attrs)
-      assert report.message == "some message"
-    end
-
-    test "create_report/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = CMS.create_report(@invalid_attrs)
-    end
-
-    test "update_report/2 with valid data updates the report" do
-      report = report_fixture()
-      assert {:ok, %Report{} = report} = CMS.update_report(report, @update_attrs)
-      assert report.message == "some updated message"
-    end
-
-    test "update_report/2 with invalid data returns error changeset" do
-      report = report_fixture()
-      assert {:error, %Ecto.Changeset{}} = CMS.update_report(report, @invalid_attrs)
-      assert report == CMS.get_report!(report.id)
-    end
-
-    test "delete_report/1 deletes the report" do
-      report = report_fixture()
-      assert {:ok, %Report{}} = CMS.delete_report(report)
-      assert_raise Ecto.NoResultsError, fn -> CMS.get_report!(report.id) end
-    end
-
-    test "change_report/1 returns a report changeset" do
-      report = report_fixture()
-      assert %Ecto.Changeset{} = CMS.change_report(report)
-    end
-  end
-
-  describe "view" do
-    alias Prequest.CMS.View
-
-    @valid_attrs %{liked?: true}
-    @update_attrs %{liked?: false}
-    @invalid_attrs %{article_id: nil, user_id: nil}
-
-    def id_map do
-      {:ok, user} = Accounts.create_user(%{username: "felipelincoln"})
-
-      {:ok, article} =
-        CMS.create_article(%{title: "a", cover: "a", source: "a", user_id: user.id})
-
-      %{user_id: user.id, article_id: article.id}
-    end
-
-    def view_fixture(attrs \\ %{}) do
-      {:ok, view} =
-        attrs
-        |> Map.merge(id_map())
-        |> Enum.into(@valid_attrs)
-        |> CMS.create_view()
-
-      view
-    end
-
-    test "list_view/0 returns all view" do
-      view = view_fixture()
-      assert CMS.list_view() == [view]
-    end
-
-    test "get_view!/1 returns the view with given id" do
-      view = view_fixture()
-      assert CMS.get_view!(view.id) == view
-    end
-
-    test "create_view/1 with valid data creates a view" do
-      attrs = Map.merge(@valid_attrs, id_map())
-      assert {:ok, %View{} = view} = CMS.create_view(attrs)
-      assert view.liked? == true
-    end
-
-    test "create_view/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = CMS.create_view(@invalid_attrs)
-    end
-
-    test "update_view/2 with valid data updates the view" do
-      view = view_fixture()
-      assert {:ok, %View{} = view} = CMS.update_view(view, @update_attrs)
-      assert view.liked? == false
-    end
-
-    test "update_view/2 with invalid data returns error changeset" do
-      view = view_fixture()
-      assert {:error, %Ecto.Changeset{}} = CMS.update_view(view, @invalid_attrs)
-      assert view == CMS.get_view!(view.id)
-    end
-
-    test "delete_view/1 deletes the view" do
-      view = view_fixture()
-      assert {:ok, %View{}} = CMS.delete_view(view)
-      assert_raise Ecto.NoResultsError, fn -> CMS.get_view!(view.id) end
-    end
-
-    test "change_view/1 returns a view changeset" do
-      view = view_fixture()
-      assert %Ecto.Changeset{} = CMS.change_view(view)
-    end
-  end
-
-  describe "topics" do
-    alias Prequest.CMS.Topic
-
-    @valid_attrs %{name: "some name"}
-    @update_attrs %{name: "some updated name"}
-    @invalid_attrs %{name: nil}
-
-    def topic_fixture(attrs \\ %{}) do
-      {:ok, topic} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> CMS.create_topic()
-
-      topic
-    end
-
-    test "list_topics/0 returns all topics" do
-      topic = topic_fixture()
-      assert CMS.list_topics() == [topic]
-    end
-
-    test "get_topic!/1 returns the topic with given id" do
-      topic = topic_fixture()
-      assert CMS.get_topic!(topic.id) == topic
-    end
-
-    test "create_topic/1 with valid data creates a topic" do
-      assert {:ok, %Topic{} = topic} = CMS.create_topic(@valid_attrs)
-      assert topic.name == "some name"
-    end
-
-    test "create_topic/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = CMS.create_topic(@invalid_attrs)
-    end
-
-    test "update_topic/2 with valid data updates the topic" do
-      topic = topic_fixture()
-      assert {:ok, %Topic{} = topic} = CMS.update_topic(topic, @update_attrs)
-      assert topic.name == "some updated name"
-    end
-
-    test "update_topic/2 with invalid data returns error changeset" do
-      topic = topic_fixture()
-      assert {:error, %Ecto.Changeset{}} = CMS.update_topic(topic, @invalid_attrs)
-      assert topic == CMS.get_topic!(topic.id)
-    end
-
-    test "delete_topic/1 deletes the topic" do
-      topic = topic_fixture()
-      assert {:ok, %Topic{}} = CMS.delete_topic(topic)
-      assert_raise Ecto.NoResultsError, fn -> CMS.get_topic!(topic.id) end
-    end
-
-    test "change_topic/1 returns a topic changeset" do
-      topic = topic_fixture()
-      assert %Ecto.Changeset{} = CMS.change_topic(topic)
+      refute CMS.preload_article!(article, :topics).topics == []
     end
   end
 end
