d0525fa92d3b403a26d44a381d8277653acdba74
Felipe Lincoln <felipelincoln@usp.br>
2020-10-02 10:45:01 -0300

    purge unsued tailwind css

--- lib/prequest_web/live/home_live.ex
+++ lib/prequest_web/live/home_live.ex
@@ -11,7 +11,7 @@ defmodule PrequestWeb.HomeLive do
 
   def render(assigns) do
     ~L"""
-    homepage
+    <h1 class="text-orange-500 text-5xl font-bold text-center">homepage</h1>
     """
   end
 end
