050a947e81fd7b9008fad741c9c7a21098c80cd5
Felipe Lincoln <felipelincoln@usp.br>
2020-11-03 09:30:09 -0300

    enhance documentation

--- lib/prequest/manage/topic/color.ex
+++ lib/prequest/manage/topic/color.ex
@@ -1,5 +1,18 @@
 defmodule Prequest.Manage.Topic.Color do
   @moduledoc """
+  A custom color manager.
+
+  To add a new color just add a `from/1'` clause matching the topic name.
+
+      # lib/prequest/manage/topic/color.ex
+      # Example of adding a color for the "elixir" tag.
+      ...
+      def from("elixir"), do: "bg-purple-900" # new clause
+      def from(_), do: random()
+      ...
+
+  The new custom clause must be declared before `from(_)` clause.
+
   Color reference: [TailwindCSS](https://tailwindcss.com/docs/background-color)
   """
 
@@ -7,24 +20,25 @@ defmodule Prequest.Manage.Topic.Color do
   @values [300, 400, 500, 600, 700, 800, 900]
 
   @doc """
-  Generate a random class for tailwind's background color.
+  Generate a random class for background color.
 
-  # Examples
+  ## Examples
 
       iex> Color.random()
       "bg-blue-500"
 
   """
+  @spec random() :: String.t()
   def random do
     "bg-#{Enum.random(@names)}-#{Enum.random(@values)}"
   end
 
   @doc """
-  Obtain a tailwind's background color class for a topic.
+  Get a class for background color for the given topic.
 
-  If the topic name is not declared as a from/1 clause, it wil generate a random class using random/0.
+  If the topic name is not declared as a `from/1` clause, it wil generate a random class using `random/0`.
 
-  # Examples
+  ## Examples
 
       iex> Color.from("elixir")
       "bg-purple-900"
@@ -33,6 +47,7 @@ defmodule Prequest.Manage.Topic.Color do
       "bg-green-300" # random
 
   """
+  @spec from(String.t()) :: String.t()
   def from("elixir"), do: "bg-purple-900"
   def from("others"), do: "bg-gray-400"
   def from(_), do: random()
