bd15343ae4be2e39b8a51d6cc02ccb366cf6cba3
Felipe Lincoln <felipelincoln@usp.br>
2020-11-05 09:50:06 -0300

    implement infinity scroll

--- lib/prequest_web/live/home_live.ex
+++ lib/prequest_web/live/home_live.ex
@@ -10,16 +10,35 @@ defmodule PrequestWeb.HomeLive do
   import Prequest.Helpers, only: [preload!: 2]
 
   def mount(_params, _session, socket) do
-    feed =
-      Article
-      |> Feed.build()
-      |> Feed.page(0)
+    source = Article
+    build = Feed.build(source)
+    feed = build |> Feed.page(0)
 
-    {:ok, assign(socket, :feed, feed)}
+    socket =
+      socket
+      |> assign(:source, source)
+      |> assign(:build, build)
+      |> assign(:feed, feed)
+      |> assign(:update, "replace")
+
+    {:ok, socket, temporary_assigns: [feed: nil]}
+  end
+
+  def handle_event("load", %{"page" => page}, socket) do
+    feed =
+      socket.assigns.build
+      |> Feed.page(String.to_integer(page) + 1)
+
+    socket =
+      socket
+      |> assign(:feed, feed)
+      |> assign(:update, "append")
+
+    {:noreply, socket}
   end
 
   def handle_event("toggle_filter", %{"topic" => new_filter}, socket) do
-    current_filter = get_filter(socket.assigns.feed)
+    current_filter = get_filter(socket.assigns.build)
 
     filter =
       case new_filter in current_filter do
@@ -27,18 +46,16 @@ defmodule PrequestWeb.HomeLive do
         false -> [new_filter | current_filter]
       end
 
-    feed =
-      Article
-      |> Feed.build(filter)
-      |> Feed.page(0)
+    build = Feed.build(socket.assigns.source, filter)
+    feed = build |> Feed.page(0)
 
-    {:noreply, assign(socket, :feed, feed)}
-  end
+    socket =
+      socket
+      |> assign(:build, build)
+      |> assign(:feed, feed)
+      |> assign(:update, "replace")
 
-  defp get_filter(feed) do
-    feed.__meta__
-    |> Map.get(:filter, {:topics, []})
-    |> elem(1)
+    {:noreply, socket}
   end
 
   # list of {article title, inserted_at}
@@ -68,5 +85,11 @@ defmodule PrequestWeb.HomeLive do
   defp others_topic(topics, count, name \\ "others") do
     limited_count = Enum.reduce(topics, 0, fn {n, _}, acc -> n + acc end)
     [{count - limited_count, %{name: name}}]
+  end
+
+  defp get_filter(feed) do
+    feed.__meta__
+    |> Map.get(:filter, {:topics, []})
+    |> elem(1)
   end
 end
