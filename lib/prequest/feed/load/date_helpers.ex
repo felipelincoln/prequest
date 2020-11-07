e35627641a11ee09f95d92a5c735b2d1bef909c4
Felipe Lincoln <felipelincoln@usp.br>
2020-11-07 10:26:14 -0300

    fix leap year bug

--- lib/prequest/feed/load/date_helpers.ex
+++ lib/prequest/feed/load/date_helpers.ex
@@ -4,17 +4,17 @@ defmodule Prequest.Feed.Load.DateHelpers do
   def get_months_ago(n) do
     [
       Date.utc_today()
-      |> first_day()
       |> months_ago(n)
+      |> first_day()
       |> to_naive_dt(:sunrise),
       Date.utc_today()
-      |> last_day()
       |> months_ago(n)
+      |> last_day()
       |> to_naive_dt(:sunset)
     ]
   end
 
-  defp months_ago(%Date{} = date, n) do
+  def months_ago(%Date{} = date, n) do
     # years to withdraw
     years = div(n, 12) + ((rem(n, 12) >= date.month and 1) || 0)
     # months to withdraw
@@ -23,21 +23,26 @@ defmodule Prequest.Feed.Load.DateHelpers do
     %{date | year: date.year - years, month: date.month - months}
   end
 
-  defp first_day(%Date{} = date) do
+  def first_day(%Date{} = date) do
     %{date | day: 1}
   end
 
-  defp last_day(%Date{} = date) do
+  def last_day(%Date{} = date) do
     %{date | day: Date.days_in_month(date)}
   end
 
-  defp to_naive_dt(%Date{year: year, month: month, day: day}, :sunrise) do
-    {:ok, date} = NaiveDateTime.new(year, month, day, 0, 0, 0)
-    date
-  end
+  def to_naive_dt(%Date{year: year, month: month, day: day} = date, sun) do
+    time =
+      case sun do
+        :sunrise -> [0, 0, 0]
+        :sunset -> [23, 59, 59]
+      end
 
-  defp to_naive_dt(%Date{year: year, month: month, day: day}, :sunset) do
-    {:ok, date} = NaiveDateTime.new(year, month, day, 23, 59, 59)
-    date
+    case apply(&NaiveDateTime.new/6, [year, month, day] ++ time) do
+      {:ok, naive_dt} -> naive_dt
+      {:error, _reason} ->
+        %{date | day: day - 1}
+        |> to_naive_dt(sun)
+    end
   end
 end
