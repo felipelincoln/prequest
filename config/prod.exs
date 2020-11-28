0eb7219b9d2e0610841fcbd0e8b8333f2f7ae897
Felipe Lincoln <felipelincoln@usp.br>
2020-11-28 10:40:03 -0300

    update domain

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :prequest, PrequestWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "prequest.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "prequest.co", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
