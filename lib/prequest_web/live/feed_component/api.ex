7586d0253888774a78d0cafd6fc78a2c2799f82e
Felipe Lincoln <felipelincoln@usp.br>
2020-12-05 16:43:21 -0300

    fix credo complexity warning

--- lib/prequest_web/live/feed_component/api.ex
+++ lib/prequest_web/live/feed_component/api.ex
@@ -55,20 +55,21 @@ defmodule PrequestWeb.FeedComponent.API do
   end
 
   def get_month_name(n) do
-    case n do
-      1 -> "January"
-      2 -> "February"
-      3 -> "March"
-      4 -> "April"
-      5 -> "May"
-      6 -> "June"
-      7 -> "July"
-      8 -> "August"
-      9 -> "September"
-      10 -> "October"
-      11 -> "November"
-      12 -> "December"
-    end
+    [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ]
+    |> Enum.at(n - 1)
   end
 
   defp get_date(range) do
